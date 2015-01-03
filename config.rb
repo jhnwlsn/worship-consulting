c18cb11d219e6fd08797e7e4e1460dcd0c664eb0
John Wilson <john@j-wilson.com>
2015-01-03 11:16:45 -0500

    Add deploy gem

--- config.rb
+++ config.rb
@@ -1,29 +1,48 @@
-activate :automatic_image_sizes
-activate :directory_indexes
+# --------------------------------------
+#   Config
+# --------------------------------------
+
+# ----- Middleman ----- #
+
 activate :livereload
+
+# ----- Assets ----- #
+
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+
+# ----- Images ----- #
+
+activate :automatic_image_sizes
+
+# ----- Directories for assets ----- #
+
+activate :directory_indexes
+
+# --------------------------------------
+#   Production
+# --------------------------------------
+
+# ----- Prefixing ----- #
 
 activate :autoprefixer do |config|
   config.browsers = ['last 2 versions', 'Explorer >= 9']
   config.cascade = false
 end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+# ----- Build ----- #
 
-ignore 'assets/javascripts/vendor/*'
-
-set :css_dir, 'assets/stylesheets'
-set :js_dir, 'assets/javascripts'
-set :images_dir, 'assets/images'
-set :fonts_dir, 'assets/fonts'
-
-# Build-specific configuration
 configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :relative_assets
+  activate :move
+  set :relative_links, true
+end
+
+# ----- Deploying ----- #
+
+activate :deploy do |deploy|
+  deploy.method = :git
 end
